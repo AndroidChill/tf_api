12b589e6a14fda19f2866420d8bcfcbfbb2465d7
AndroidChill <androidChill@yandex.ru>
2022-01-04 14:23:10 +0300

    df

--- src/__init__.py
+++ src/__init__.py
@@ -16,9 +16,9 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SECRET_KEY='SECRET_KEY',
             SQLALCHEMY_DATABASE_URI="sqlite:///tf_db.db",
-            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+            JWT_SECRET_KEY='JWT_SECRET_KEY',
             JWT_ACCESS_TOKEN_EXPIRES=36600,
             SQLALCHEMY_TRACK_MODIFICATIONS = False,
             SWAGGER={
